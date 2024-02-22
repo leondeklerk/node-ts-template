14439ead96224f74a612c912df02f21e5613b1da
Leon de Klerk <developer@leondeklerk.com>
2024-02-22 23:14:36 +0100

    Update pre-commit hook
    Signed-off-by: Leon de Klerk <developer@leondeklerk.com>

--- scripts/prepare.js
+++
@@ -1,6 +0,0 @@
-const preventInstall = process.env.CI !== undefined || process.env.NODE_ENV === "production";
-if (!preventInstall) {
-	import("husky").then((husky) => {
-		husky.install();
-	});
-}
